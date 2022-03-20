af2d052aa85be295a3b906e3e7f79272b2a17517
Sebastian Ospina <juansebastian200103@gmail.com>
2022-03-20 19:58:34 -0400

    PARTE4.3

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -1,5 +1,9 @@
 package edu.eci.arsw.collabpaint;
 
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,13 +14,26 @@ import edu.eci.arsw.collabpaint.model.Point;
 
 @Controller
 public class STOMPMessagesHandler {
-	
+
 	@Autowired
 	SimpMessagingTemplate msgt;
-    
-	@MessageMapping("/newpoint.{numdibujo}")    
-	public void handlePointEvent(Point pt,@DestinationVariable String numdibujo) throws Exception {
-		System.out.println("Nuevo punto recibido en el servidor!:"+pt);
-		msgt.convertAndSend("/topic/newpoint."+numdibujo, pt);
+
+	private HashMap<String, ConcurrentLinkedQueue<Point>> points = new HashMap<>();
+
+	@MessageMapping("/newpoint.{numdibujo}")
+	public synchronized void handlePointEvent(Point pt, @DestinationVariable String numdibujo) throws Exception {
+
+		System.out.println("Nuevo punto recibido en el servidor!:" + pt);
+		msgt.convertAndSend("/topic/newpoint." + numdibujo, pt);
+			if (!points.containsKey(numdibujo))
+				points.put(numdibujo, new ConcurrentLinkedQueue<>());
+			ConcurrentLinkedQueue<Point> p = points.get(numdibujo);
+			if (!p.contains(pt)) {
+				p.add(pt);
+			}
+			if (p.size() == 4) {
+				msgt.convertAndSend("/topic/newpolygon." + numdibujo, p);
+				p.clear();
+			}
 	}
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,24 +2,39 @@ var app = (function () {
 
     var urlDibujo = 0;
 
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
+    class Point {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
     }
-    
+
     var stompClient = null;
 
-    var addPointToCanvas = function (point) {        
+    var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
-    
+
+    var addPoligonToCanvas = function (points) {
+        console.log(points[1].x);
+        var data = points;
+        var canvas = document.getElementById("canvas");
+        var line = canvas.getContext("2d");
+        line.beginPath();
+        line.fillStyle = '#f00';
+        for (i = 0; i < data.length; i++) {
+            line.moveTo(data[i].x, data[i].y);
+            var j = i + 1 < data.length ? i + 1 : 0;
+            line.lineTo(data[j].x, data[j].y);
+        }
+        line.closePath();
+        line.fill();
+        line.stroke();
+    }
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
@@ -29,9 +44,9 @@ var app = (function () {
         };
     };
 
-    var publicPointAtTopic = function (point){
-        stompClient.send("/app"+urlDibujo, {}, JSON.stringify(point));
-        console.log("Se publico el punto "+point);
+    var publicPointAtTopic = function (point) {
+        stompClient.send("/app" + urlDibujo, {}, JSON.stringify(point));
+        console.log("Se publico el punto " + point);
     };
 
 
@@ -39,41 +54,50 @@ var app = (function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-        
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe("/topic"+urlDibujo, function (eventbody) {
-                var point=JSON.parse(eventbody.body);
-                addPointToCanvas(point);
+            stompClient.subscribe("/topic" + urlDibujo, function (eventbody) {
+                console.log(JSON.parse(eventbody.body));
+                if (urlDibujo.includes("newpoint")) {
+                    var point = JSON.parse(eventbody.body);
+                    addPointToCanvas(point);
+                }
+                else{
+  
+                    addPoligonToCanvas(JSON.parse(eventbody.body));
+                }
             });
         });
 
     };
-    
-    
+
+
 
     return {
 
         connect: function (dibujo) {
             var can = document.getElementById("canvas");
             //websocket connection
-            urlDibujo = '/newpoint.'+dibujo;
+            var selecion = document.getElementById("conexion")
+            urlDibujo = selecion.value + dibujo;
             connectAndSubscribe();
-            alert("Te conectaste al dibujo No"+ dibujo);
-            if(window.PointerEvent){
-                can.addEventListener("pointerdown",function(evt){
-                    var point = getMousePosition(evt);
-                    console.log(point);
-                    addPointToCanvas(point);
-                    publicPointAtTopic(point)
-                });
+            alert("Te conectaste al "+selecion[selecion.selectedIndex].innerHTML+" No" + dibujo);
+            if (urlDibujo.includes("newpoint")) {
+                if (window.PointerEvent) {
+                    can.addEventListener("pointerdown", function (evt) {
+                        var point = getMousePosition(evt);
+                        console.log(point);
+                        addPointToCanvas(point);
+                        publicPointAtTopic(point)
+                    });
+                }
             }
         },
 
-        publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            console.info("publishing point at "+pt);
+        publishPoint: function (px, py) {
+            var pt = new Point(px, py);
+            console.info("publishing point at " + pt);
             addPointToCanvas(pt);
             publicPointAtTopic(pt)
             //publicar el evento
